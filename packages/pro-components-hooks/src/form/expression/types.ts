572500863f20ad1369deeaa8857b440ecf2f25df
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-06-05 16:55:46 +0800

    feat(Form): 优化 useCompile

---
+++ packages/pro-components-hooks/src/form/expression/types.ts
@@ -0,0 +1,15 @@
+// eslint-disable-next-line ts/ban-types
+type StringExpression = ({} & `{{${string}}}`)
+export type MaybeExpression<T> = T extends (infer U)[]
+  ? (MaybeExpression<U>)[]
+  : T extends object
+    ? { [K in keyof T]: MaybeExpression<T[K]> }
+    : T | StringExpression
+
+export type ExcludeExpression<T> = T extends (infer U)[]
+  ? ExcludeExpression<U>[] // 递归处理数组项
+  : T extends object
+    ? { [K in keyof T]: ExcludeExpression<T[K]> } // 递归处理对象
+    : T extends StringExpression
+      ? Exclude<T, StringExpression>
+      : T
--- packages/pro-components-hooks/src/form/expression/useCompile.ts
+++ packages/pro-components-hooks/src/form/expression/useCompile.ts
@@ -1,5 +1,7 @@
-import { isString } from 'lodash-es'
-import { type ComputedRef, computed, unref } from 'vue-demi'
+import { isArray, isPlainObject, isString } from 'lodash-es'
+import type { ComputedRef, Ref } from 'vue-demi'
+import { computed, unref } from 'vue-demi'
+import type { ExcludeExpression } from './types'
 
 const expressionReg = /\{\{([\s\S]*)\}\}/
 
@@ -20,18 +22,32 @@ export interface UseCompileOptions {
   scope?: Record<string, any>
 }
 
-export function useCompile<T = any>(
-  value: string | Record<string, any> | ComputedRef<Record<string, any>>,
+export function useCompile<T extends (string | Record<string, any> | Ref<any> | ComputedRef<Record<string, any>>)>(
+  value: T,
   options: UseCompileOptions = {},
-): ComputedRef<T> {
+): ComputedRef<ExcludeExpression<T>> {
   const { scope = {} } = options
   return computed(() => {
     if (isString(value))
-      return compile(value, scope) as T
+      return compile(value, scope)
+
     const records = unref(value)
-    const ret: any = {}
-    for (const key in records)
-      ret[key] = compile(records[key], scope)
-    return ret as T
+    if (records === undefined)
+      return records
+
+    if (!isArray(records) && !isPlainObject(records))
+      return records
+
+    const traverse = (data: any) => {
+      if (!isArray(data) && !isPlainObject(data))
+        return data
+      const ret: any = isArray(data) ? [] : {}
+      for (const key in data) {
+        const val = traverse(data[key])
+        ret[key] = compile(val, scope)
+      }
+      return ret
+    }
+    return traverse(records) as T
   })
 }
