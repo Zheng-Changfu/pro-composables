3fbb909b2fe0cf7a25a2c41363d1f12f7fea7af6
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-06-05 19:30:14 +0800

    type: 类型修复

--- packages/pro-components-hooks/src/form/expression/types.ts
+++ packages/pro-components-hooks/src/form/expression/types.ts
@@ -2,13 +2,31 @@ type StringExpression = `{{${string}}}`
 export type MaybeExpression<T> = T extends (infer U)[]
   ? (MaybeExpression<U>)[]
   : T extends object
-    ? { [K in keyof T]: MaybeExpression<T[K]> }
+    ? T extends RegExp
+      ? T | StringExpression
+      : { [K in keyof T]: MaybeExpression<T[K]> }
     : T | StringExpression
 
 export type ExcludeExpression<T> = T extends (infer U)[]
-  ? (ExcludeExpression<U>)[] // 递归处理数组项
+  ? (ExcludeExpression<U>)[]
   : T extends object
-    ? { [K in keyof T]: ExcludeExpression<T[K]> } // 递归处理对象
+    ? T extends RegExp
+      ? T
+      : { [K in keyof T]: ExcludeExpression<T[K]> }
     : T extends StringExpression
-      ? Exclude<T, StringExpression>
+      ? never
       : T
+
+export interface RuleItem {
+  pattern?: RegExp
+}
+export type FormItemRule = RuleItem
+
+type E = MaybeExpression<FormItemRule | FormItemRule[]>
+
+function c(a: FormItemRule | FormItemRule[]) {
+  console.log(a, 'a')
+}
+
+const e = {} as ExcludeExpression<E>
+c(e)
