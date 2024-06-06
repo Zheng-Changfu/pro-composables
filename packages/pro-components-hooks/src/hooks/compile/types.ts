b449637c1528d1850fba750d7b47bce339988ee0
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-06-06 22:11:37 +0800

    types(useCompile): 优化类型

--- packages/pro-components-hooks/src/hooks/compile/types.ts
+++ packages/pro-components-hooks/src/hooks/compile/types.ts
@@ -1,15 +1,11 @@
 type StringExpression = `{{${string}}}`
-// eslint-disable-next-line unused-imports/no-unused-vars
-type IsTuple<T> = T extends [infer A, ...infer B] ? true : false
 
 type IsSpecialObject<T> = T extends RegExp | Date | Function | Map<any, any> | Set<any> | WeakMap<any, any> | WeakSet<any>
   ? true
   : false
 
 export type MaybeExpression<T> = T extends (infer U)[]
-  ? IsTuple<T> extends true
-    ? { [K in keyof T]: MaybeExpression<T[K]> }
-    : (MaybeExpression<U>)[]
+  ? (MaybeExpression<U>)[]
   : IsSpecialObject<T> extends true
     ? T | StringExpression
     : T extends object
@@ -17,9 +13,7 @@ export type MaybeExpression<T> = T extends (infer U)[]
       : T | StringExpression
 
 export type ExcludeExpression<T> = T extends (infer U)[]
-  ? IsTuple<T> extends true
-    ? { [K in keyof T]: ExcludeExpression<T[K]> }
-    : (ExcludeExpression<U>)[]
+  ? (ExcludeExpression<U>)[]
   : IsSpecialObject<T> extends true
     ? T
     : T extends object
