15eb2afc87a33bdad6673c3ee0d1bf21f55b8cfb
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-06-12 11:54:26 +0800

    fix(types): 优化类型

--- packages/pro-components-hooks/src/hooks/compile/types.ts
+++ packages/pro-components-hooks/src/hooks/compile/types.ts
@@ -1,3 +1,5 @@
+import type { CSSProperties, Ref } from 'vue-demi'
+
 type StringExpression = `{{${string}}}`
 type IsPlainString<T> = T extends string
   ? T extends StringExpression
@@ -5,7 +7,25 @@ type IsPlainString<T> = T extends string
     : true
   : false
 
-type IsSpecialObject<T> = T extends RegExp | Date | Function | Map<any, any> | Set<any> | WeakMap<any, any> | WeakSet<any>
+type IsNumber<T> = T extends number ? true : false
+type IsBoolean<T> = T extends boolean ? true : false
+
+type IsBasicType<T> = IsPlainString<T> extends true
+  ? true
+  : IsNumber<T> extends true
+    ? true
+    : IsBoolean<T> extends true
+      ? true
+      : false
+
+type IsSpecialObject<T> = T extends
+  | RegExp
+  | Date
+  | Function
+  | Map<any, any>
+  | Set<any>
+  | WeakMap<any, any>
+  | WeakSet<any>
   ? true
   : false
 
@@ -13,7 +33,7 @@ export type MaybeExpression<T> = T extends (infer U)[]
   ? (MaybeExpression<U>)[]
   : IsSpecialObject<T> extends true
     ? T | StringExpression
-    : IsPlainString<T> extends true
+    : IsBasicType<T> extends true
       ? T | StringExpression
       : T extends object
         ? { [K in keyof T]: MaybeExpression<T[K]> }
@@ -23,10 +43,18 @@ export type ExcludeExpression<T> = T extends (infer U)[]
   ? (ExcludeExpression<U>)[]
   : IsSpecialObject<T> extends true
     ? T
-    : IsPlainString<T> extends true
+    : IsBasicType<T> extends true
       ? T
       : T extends object
         ? { [K in keyof T]: ExcludeExpression<T[K]> }
         : T extends StringExpression
           ? never
           : T
+
+// eslint-disable-next-line ts/ban-types
+export type AnimationIterationCount = 'infinite' | (string & {}) | (number & {})
+
+function t(t: AnimationIterationCount) {
+  console.log(t)
+}
+t({} as any as ExcludeExpression<MaybeExpression<AnimationIterationCount>>)
