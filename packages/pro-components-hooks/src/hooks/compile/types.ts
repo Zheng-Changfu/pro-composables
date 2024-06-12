60b8ac2f24b7581ac9e7065b61467c48c1410f57
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-06-12 11:17:54 +0800

    fix(types): 优化类型

--- packages/pro-components-hooks/src/hooks/compile/types.ts
+++ packages/pro-components-hooks/src/hooks/compile/types.ts
@@ -1,5 +1,5 @@
 type StringExpression = `{{${string}}}`
-
+type IsString<T> = T extends string ? true : false
 type IsSpecialObject<T> = T extends RegExp | Date | Function | Map<any, any> | Set<any> | WeakMap<any, any> | WeakSet<any>
   ? true
   : false
@@ -8,16 +8,20 @@ export type MaybeExpression<T> = T extends (infer U)[]
   ? (MaybeExpression<U>)[]
   : IsSpecialObject<T> extends true
     ? T | StringExpression
-    : T extends object
-      ? { [K in keyof T]: MaybeExpression<T[K]> }
-      : T | StringExpression
+    : IsString<T> extends true
+      ? T | StringExpression
+      : T extends object
+        ? { [K in keyof T]: MaybeExpression<T[K]> }
+        : T | StringExpression
 
 export type ExcludeExpression<T> = T extends (infer U)[]
   ? (ExcludeExpression<U>)[]
   : IsSpecialObject<T> extends true
     ? T
-    : T extends object
-      ? { [K in keyof T]: ExcludeExpression<T[K]> }
-      : T extends StringExpression
-        ? never
-        : T
+    : IsString<T> extends true
+      ? T
+      : T extends object
+        ? { [K in keyof T]: ExcludeExpression<T[K]> }
+        : T extends StringExpression
+          ? never
+          : T
