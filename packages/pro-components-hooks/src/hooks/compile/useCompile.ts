91f292f9a96d6d6416a847cf3954b6ae69a65bc0
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-07-05 14:08:46 +0800

    fix: 修复 proxy 数据编译不生效问题

--- packages/pro-components-hooks/src/hooks/compile/useCompile.ts
+++ packages/pro-components-hooks/src/hooks/compile/useCompile.ts
@@ -1,6 +1,6 @@
 import { isArray, isPlainObject, isString } from 'lodash-es'
 import type { ComputedRef, Ref, UnwrapRef } from 'vue-demi'
-import { computed, isRef, unref } from 'vue-demi'
+import { computed, isProxy, isRef, unref } from 'vue-demi'
 import type { ExcludeExpression } from './types'
 
 const expressionReg = /\{\{([\s\S]*)\}\}/
@@ -51,7 +51,7 @@ export function compile<T = any>(source: T, scope: Record<string, any>): Exclude
     return source as any
 
   const traverse = (data: any) => {
-    if (!isArray(data) && !isPlainObject(data))
+    if (!isArray(data) && !isProxy(data) && !isPlainObject(data))
       return data
     const ret: any = isArray(data) ? [] : {}
     for (const key in data) {
