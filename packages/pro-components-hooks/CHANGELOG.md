7a13e38e8917a9e810cbc1079326d5bd8358d52e
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-07-21 22:01:40 +0800

    types: 导出 ExpressionScope

--- packages/pro-components-hooks/CHANGELOG.md
+++ packages/pro-components-hooks/CHANGELOG.md
@@ -1,5 +1,11 @@
 # pro-components-hooks
 
+## 0.0.68
+
+### Patch Changes
+
+- feat:release@0.0.68
+
 ## 0.0.67
 
 ### Patch Changes
--- packages/pro-components-hooks/src/form/field/index.ts
+++ packages/pro-components-hooks/src/form/field/index.ts
@@ -1,4 +1,5 @@
 export * from './types'
 export * from './createField'
 export * from './createArrayField'
+export type { ExpressionScope } from './scope'
 export { useInjectParentFieldContext, useInjectFieldContext } from './context'
