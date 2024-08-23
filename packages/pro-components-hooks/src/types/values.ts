f31d6234e8cc3f28703ff06f18cede21968a312e
liyuan26 <liyuan26@daojia-inc.com>
2024-08-23 18:22:44 +0800

    types: 提供通用类型

--- packages/pro-components-hooks/src/types/values.ts
+++ packages/pro-components-hooks/src/types/values.ts
@@ -1,3 +1,13 @@
 import type { ExtractObjectPaths } from './keys'
 
-export type ExtractPathValue<Values, Path extends ExtractObjectPaths<Values>> = any
+export type ExtractPathValue<Values, Path extends ExtractObjectPaths<Values> | any> = Path extends `${infer A}.${infer B}`
+  ? A extends keyof Values
+    ? ExtractPathValue<Values[A], B>
+    : A extends `${number}`
+      ? Values extends Array<any>
+        ? ExtractPathValue<Values[number], B> | undefined
+        : never
+      : never
+  : Path extends keyof Values
+    ? Values[Path] | undefined
+    : never
