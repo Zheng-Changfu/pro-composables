368862917055530641d5539f6112b9043ced9b21
zhengchangfu <zcfpromise@163.com>
2024-12-11 22:08:19 +0800

    types: 暴露 useRequest，暴露相关类型

--- src/composables/useRequest/index.ts
+++ src/composables/useRequest/index.ts
@@ -2,3 +2,9 @@
  * 社区没有对标 ahooks@3.8.2 的 useRequest vue 版本实现，只能加班自己干了
  */
 export { useRequest } from './useRequest'
+
+export type {
+  Plugin as UseRequestPlugin,
+  Options as UseRequestOptions,
+  Service as UseRequestService,
+} from './types'
